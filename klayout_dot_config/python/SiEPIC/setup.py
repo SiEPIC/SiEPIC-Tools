618704b20602efb73c385807a30017abdfd2dd57
bpoulse <bpoulse@uwo.ca>
2017-12-16 17:59:47 -0500

    moving back to module

---
+++ klayout_dot_config/python/SiEPIC/setup.py
@@ -0,0 +1,297 @@
+import pya
+
+def registerMenuItems():
+    import os
+    from . import scripts, examples, lumerical, install
+    
+    global ACTIONS
+    count = 0
+    menu = pya.Application.instance().main_window().menu()
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "files", "INTERCONNECT_icon.png")
+    
+    s1 = "siepic_menu"
+    if not(menu.is_menu(s1)):
+        menu.insert_menu("help_menu",s1,"SiEPIC") 
+
+    s2 = "install"
+    if not(menu.is_menu(s1 + "." + s2)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + ".end" , s2, ACTIONS[count])
+    ACTIONS[count].on_triggered(install.install_dependencies)
+    ACTIONS[count].title = "Install SiEPIC Dependencies"
+    count += 1
+
+    s2 = "waveguides"
+    if not(menu.is_menu(s1 + "." + s2)):
+        menu.insert_menu(s1 + ".end" , s2, "Waveguides")
+
+    s3 = "path_to_wg"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.waveguide_from_path)
+    ACTIONS[count].title = "Path to Waveguide"
+    count += 1
+
+    s3 = "wg_to_path"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.waveguide_to_path)
+    ACTIONS[count].title = "Waveguide to Path"
+    count += 1
+
+    s3 = "measure_wg"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.waveguide_length)
+    ACTIONS[count].title = "Measure Waveguide Length"
+    count += 1
+
+    s3 = "measure_wg_diff"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.waveguide_length_diff)
+    ACTIONS[count].title = "Measure Waveguide Length Difference"
+    count += 1
+
+    s3 = "wg_heal"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.waveguide_heal)
+    ACTIONS[count].title = "Heal Waveguides"
+    count += 1
+
+    s2 = "layout"
+    if not(menu.is_menu(s1 + "." + s2)):
+        menu.insert_menu(s1 + ".end", s2, "Layout")
+
+    s3 = "auto_route"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.auto_route)
+    ACTIONS[count].title = "Automated A* Routing"
+    count += 1
+
+    s3 = "snap_component"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])  
+    ACTIONS[count].on_triggered(scripts.snap_component)
+    ACTIONS[count].title = "Snap Selected Component to Nearest Pins"
+    count += 1
+
+    s3 = "delete_top_cells"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.delete_top_cells)
+    ACTIONS[count].title = "Delete Extra Top Cells"
+    count += 1
+
+    s2 = "exlayout"
+    if not(menu.is_menu(s1 + "." + s2)):
+        menu.insert_menu(s1 +".end", s2,"Example Layouts")
+
+    s3 = "dbl_bus_ring_res"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(examples.dbl_bus_ring_res)
+    ACTIONS[count].title = "Double Bus Ring Resonator"
+    count += 1
+
+    s3 = "mzi"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(examples.mzi)
+    ACTIONS[count].title = "12 TE Mach-Zehner Interferometers - Jaspreet Jhoja"
+    count += 1
+
+    s2 = "verification"
+    if not(menu.is_menu(s1 + "." + s2)):
+        menu.insert_menu(s1 + ".end", s2, "Verification")
+
+    s3 = "compute_area"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.calculate_area)
+    ACTIONS[count].title = "Compute Area of Silicon Layers"
+    count += 1
+
+    s3 = "calibreDRC"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.calibreDRC)
+    ACTIONS[count].title = "Remote Calibre DRC"
+    count += 1
+
+    s3 = "auto_coord_extract"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.auto_coord_extract)
+    ACTIONS[count].title = "Automated Coordinate Extraction"
+    count += 1
+
+    s3 = "layout_check"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.layout_check)
+    ACTIONS[count].title = "Verification: Graphical Layout Check"
+    count += 1
+
+    s3 = "text_netlist_check"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(scripts.text_netlist_check)
+    ACTIONS[count].title = "Verification: Text and Netlist Generation"
+    count += 1
+
+    s2 = "simulation"
+    if not(menu.is_menu(s1 + "." + s2)):
+        menu.insert_menu(s1 + ".end", s2, "Simulation")
+
+    s3 = "mc_sim"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(lumerical.interconnect.monte_carlo)
+    ACTIONS[count].title = "Monte Carlo Simulation"
+    count += 1
+
+    s3 = "launch_lumerical"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(lumerical.interconnect.launch)
+    ACTIONS[count].title = "Launch Lumerical Interconnect"
+    count += 1
+
+    s3 = "update_netlist"
+    if not(menu.is_menu(s1 + "." + s2 + "." + s3)):
+        ACTIONS.append(pya.Action())
+        menu.insert_item(s1 + "." + s2 + ".end" , s3, ACTIONS[count])
+    ACTIONS[count].on_triggered(lumerical.interconnect.update_netlist)
+    ACTIONS[count].title = "Update Netlist in Lumerical Inteconnect"
+    count += 1
+    
+    if not(menu.is_menu("@toolbar.cir_sim")):
+        ACTIONS.append(pya.Action())
+        menu.insert_item("@toolbar.end", "cir_sim", ACTIONS[count])
+    ACTIONS[count].title = "Circuit \nSimulation"
+    ACTIONS[count].on_triggered(lumerical.interconnect.launch)
+    ACTIONS[count].icon = path
+    count += 1
+
+    if not(menu.is_menu("@toolbar.cir_sim.mc_sim")):
+        ACTIONS.append(pya.Action())
+        menu.insert_item("@toolbar.cir_sim.end", "mc_sim", ACTIONS[count])
+    ACTIONS[count].title = "INTERCONNECT Monte Carlo Simulations"
+    ACTIONS[count].on_triggered(lumerical.interconnect.monte_carlo)
+    ACTIONS[count].icon = path
+    count += 1
+
+    if not(menu.is_menu("@toolbar.cir_sim.launch_lumerical")):
+        ACTIONS.append(pya.Action())
+        menu.insert_item("@toolbar.cir_sim.end", "launch_lumerical", ACTIONS[count])
+    ACTIONS[count].title = "INTERCONNECT Circuit Simulation"
+    ACTIONS[count].on_triggered(lumerical.interconnect.launch)
+    ACTIONS[count].icon = path
+    count += 1
+
+    if not(menu.is_menu("@toolbar.cir_sim.update_netlist")):
+        ACTIONS.append(pya.Action())
+        menu.insert_item("@toolbar.cir_sim.end", "update_netlist", ACTIONS[count])
+    ACTIONS[count].title = "INTERCONNECT Update Netlist"
+    ACTIONS[count].on_triggered(lumerical.interconnect.update_netlist)
+    ACTIONS[count].icon = path
+
+def registerKeyBindings():
+    import os
+
+    config = pya.Application.instance().get_config('key-bindings')
+    if config == '':
+        print('WARNING: get_config(key-bindings) returned null')
+        mapping = dict()
+    else:
+        mapping = dict(item.split(":") for item in config.split(";"))
+
+    mapping['edit_menu.clear_all_rulers'] = "'Ctrl+K'"
+    mapping['edit_menu.copy'] = "'Ctrl+C'"
+    mapping['edit_menu.cut'] = "'Ctrl+X'"
+    mapping['edit_menu.paste'] = "'Ctrl+V'"
+    mapping['edit_menu.redo'] = "'Ctrl+Y'"
+    mapping['edit_menu.undo'] = "'Ctrl+Z'"
+    mapping['edit_menu.delete'] = "'Del'"
+    #  mapping['edit_menu.duplicate'] = "'Ctrl+B'"
+    mapping['edit_menu.mode_menu.move'] = "'M'"
+    mapping['edit_menu.mode_menu.ruler'] = "'R'"
+    mapping['edit_menu.mode_menu.select'] = "'S'"
+    mapping['edit_menu.mode_menu.box'] = "'B'"
+    mapping['edit_menu.mode_menu.instance'] = "'I'"
+    mapping['edit_menu.mode_menu.partial'] = "'L'"
+    mapping['edit_menu.mode_menu.path'] = "'P'"
+    mapping['edit_menu.mode_menu.polygon'] = "'G'"
+    mapping['edit_menu.mode_menu.text'] = "'X'"
+    mapping['edit_menu.select_menu.select_all'] = "'Ctrl+A'"
+    mapping['edit_menu.show_properties'] = "'Q'"
+    mapping['edit_menu.edit_options'] = "'E'"
+    mapping['edit_menu.selection_menu.change_layer'] = "'Shift+L'"
+    mapping['edit_menu.selection_menu.sel_flip_x'] = "'Shift+H'"
+    mapping['edit_menu.selection_menu.sel_flip_y'] = "'Shift+V'"
+    mapping['edit_menu.selection_menu.sel_move'] = "'Ctrl+M'"
+    #  mapping['edit_menu.selection_menu.size'] = "'Z'"
+    #  mapping['edit_menu.selection_menu.tap'] = "''"
+
+    mapping['file_menu.new_layout'] = "'Ctrl+N'"
+    mapping['file_menu.close'] = "'Ctrl+W'"
+    mapping['file_menu.open_new_panel'] = "'Ctrl+O'"
+    mapping['file_menu.open_same_panel'] = "'Ctrl+Shift+O'"
+    mapping['file_menu.save'] = "'Ctrl+S'"
+    mapping['file_menu.save_as'] = "'Ctrl+Shift+S'"
+    mapping['file_menu.screenshot'] = "'F12'"
+    #  mapping['file_menu.setup'] = "'F4'"
+
+    mapping['macros_menu.macro_development'] = "'F5'"
+
+    mapping['zoom_menu.max_hier'] = "'Shift+F'"
+    mapping['zoom_menu.select_current_cell'] = "'Shift+S'" # Display > Show as new top
+    mapping['zoom_menu.zoom_fit'] = "'F'"
+    mapping['zoom_menu.zoom_fit_sel'] = "'Shift+F2'"
+    mapping['zoom_menu.zoom_in'] = "'Return'"
+    mapping['zoom_menu.zoom_out'] = "'Shift+Return'"
+
+    mapping['siepic_menu.waveguides.path_to_wg'] = "'W'"
+    mapping['siepic_menu.waveguides.wg_to_path'] = "'Shift+W'"
+    mapping['siepic_menu.waveguides.measure_wg'] = "'Alt+Shift+W'"
+    mapping['siepic_menu.waveguides.measure_wg_diff'] = "'Shift+D'"
+    mapping['siepic_menu.waveguides.wg_heal'] = "'H'"
+    mapping['siepic_menu.waveguides.snap_component'] = "'Shift+O'"
+    mapping['siepic_menu.verification.layout_check'] = "'V'"
+    mapping['siepic_menu.verification.text_netlist_check'] = "'N'"
+
+    # turn the hash back into a config string
+    config = ''.join('{}:{};'.format(key, val) for key, val in sorted(mapping.items()))[:-1]
+    pya.Application.instance().set_config('key-bindings', config)
+    pya.Application.instance().set_config('edit-connect-angle-mode','ortho')
+    pya.Application.instance().set_config('edit-inst-angle','0')
+    pya.Application.instance().set_config('edit-move-angle-mode','diagonal')
+    pya.Application.instance().set_config('edit-snap-to-objects','true')
+    pya.Application.instance().set_config('grid-micron','0.01')
+    pya.Application.instance().set_config('edit-top-level-selection','true')
+    pya.Application.instance().set_config('inst-color','#ffcdcd')
+    pya.Application.instance().set_config('text-font','3')
+    pya.Application.instance().set_config('guiding-shape-line-width','0')
+    pya.Application.instance().set_config('rdb-marker-color','#ff0000')
+    pya.Application.instance().set_config('rdb-marker-line-width','8')
+    pya.Application.instance().set_config('default-layer-properties', os.path.join(os.path.realpath(__file__), '..', 'libraries', 'klayout_Layers_EBeam.lyp'))
