9af7433e429855a63afced304c9cef27982d80da
lukasc-ubc <lukasc@ece.ubc.ca>
2018-01-22 21:43:16 -0800

    v0.3.18

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.17'
+__version__ = '0.3.18'
 #from . import install, extend, _globals, core, examples, github, lumerical, scripts, utils, setup
 from . import install, extend, _globals, core, examples, github, scripts, utils, setup
 
