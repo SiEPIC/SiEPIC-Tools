d1d091db01a5d9d65f4a5ec1a8ba649a2a84a0db
Lukas Chrostowski <lukasc@ece.ubc.ca>
2018-01-24 00:32:41 -0800

    add component name for CML

--- klayout_dot_config/python/SiEPIC/lumerical/fdtd.py
+++ klayout_dot_config/python/SiEPIC/lumerical/fdtd.py
@@ -509,6 +509,7 @@ def generate_component_sparam(do_simulation = True, addto_CML = True, verbose = 
     t+= 'set("s parameters filename", "%s");\n' % (file_sparam)
     t+= 'set("load from file", false);\n'
     t+= 'set("passivity", "enforce");\n'
+    t+= 'set("prefix", component);\n'
     t+= 'setposition(component+"::SPAR_1",100,-100);\n'
     count=0
     for p in pins:
