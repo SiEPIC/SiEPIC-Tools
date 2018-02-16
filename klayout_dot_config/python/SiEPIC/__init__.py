6357ef20aab9d59b9e0ceb6b50acadac120c30e9
Lukas Chrostowski <lukasc@ece.ubc.ca>
2018-02-16 02:08:59 -0800

    v0.3.28: fix resize; fix verification overlapping check

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.24'
+__version__ = '0.3.28'
 #from . import install, extend, _globals, core, examples, github, lumerical, scripts, utils, setup
 from . import install, extend, _globals, core, examples, github, scripts, utils, setup
 
