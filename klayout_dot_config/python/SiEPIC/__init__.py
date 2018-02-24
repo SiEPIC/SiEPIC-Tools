1a34e664ff35f28e3d652f3fcf3e12d52bca3715
Lukas Chrostowski <lukasc@ece.ubc.ca>
2018-02-24 06:35:02 -0700

    v0.3.30: fixed; added error messages; Linux support for INTC
    fix for Python3.

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.29'
+__version__ = '0.3.30'
 #from . import install, extend, _globals, core, examples, github, lumerical, scripts, utils, setup
 from . import install, extend, _globals, core, examples, github, scripts, utils, setup
 
