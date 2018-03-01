aa75b44c26ae22e4028dab97c4722503afc9bbe6
Lukas Chrostowski <lukasc@ece.ubc.ca>
2018-03-01 01:14:38 -0800

    v0.3.36: verification fix for opt_in DFT

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.35'
+__version__ = '0.3.36'
 #from . import install, extend, _globals, core, examples, github, lumerical, scripts, utils, setup
 from . import install, extend, _globals, core, examples, github, scripts, utils, setup
 
