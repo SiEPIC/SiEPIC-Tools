2b8140e4cb4f549968fcf0f8db0a5ab387e8e5c3
lukasc-ubc <lukasc@ece.ubc.ca>
2018-03-02 03:13:18 -0800

    v0.3.37: fixes including DFT missing opt_in

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.36'
+__version__ = '0.3.37'
 #from . import install, extend, _globals, core, examples, github, lumerical, scripts, utils, setup
 from . import install, extend, _globals, core, examples, github, scripts, utils, setup
 
