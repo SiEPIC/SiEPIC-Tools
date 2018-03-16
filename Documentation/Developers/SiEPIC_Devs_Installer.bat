8a6c8be39a1d3903f9dc9dc3a2bfc43accec70a7
Lukas Chrostowski <lukasc@ece.ubc.ca>
2018-03-16 01:44:18 -0700

    windows installer from GitHub

---
+++ Documentation/Developers/SiEPIC_Devs_Installer.bat
@@ -0,0 +1,29 @@
+mkdir C:\Users\%username%\KLayout\tech
+mklink /d C:\Users\%username%\KLayout\pymacros\SiEPIC-Tools C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\klayout_dot_config\pymacros
+
+mklink /d C:\Users\%username%\KLayout\icons C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\klayout_dot_config\icons
+
+mklink /d C:\Users\%username%\KLayout\python\SiEPIC C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\klayout_dot_config\python\SiEPIC
+
+mklink /d C:\Users\%username%\KLayout\python\svgwrite C:\users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\svgwrite
+
+mklink /d C:\Users\%username%\KLayout\python\requests C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\requests
+
+mklink C:\Users\%username%\KLayout\python\pyparsing.py C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\pyparsing.py
+
+mklink /d C:\Users\%username%\KLayout\python\certifi C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\certifi
+
+mklink /d C:\Users\%username%\KLayout\python\chardet C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\chardet
+
+mklink /d C:\Users\%username%\KLayout\python\idna C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\idna
+
+mklink /d C:\Users\%username%\KLayout\python\numpy C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Windows_Python_packages_for_KLayout\python\numpy
+
+mklink /d C:\Users\%username%\KLayout\python\urllib3 C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\urllib3c
+
+mklink /d C:\Users\%username%\KLayout\python\qtgr C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\qtgr
+
+mklink /d C:\Users\%username%\KLayout\python\gr C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\gr
+
+mklink /d C:\Users\%username%\KLayout\python\gr3 C:\Users\%username%\Documents\GitHub\SiEPIC-Tools\Python_packages_for_KLayout\python\gr3
+
