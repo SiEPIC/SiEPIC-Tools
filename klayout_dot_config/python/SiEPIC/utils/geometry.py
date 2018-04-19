a4dc68c6fa7587503ae38911ee6b2d586b2791e4
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-04-18 23:34:16 -0400

    layout helper tools and advanced bezier curves

---
+++ klayout_dot_config/python/SiEPIC/utils/geometry.py
@@ -0,0 +1,372 @@
+"""Geometry module
+
+Right now it contains helper functions for the bezier curves.
+
+Author: Thomas Ferreira de Lima @thomaslima
+
+"""
+import numpy as np
+from numpy import sqrt
+from . import sample_function
+
+MAGIC_NUMBER = 15.0
+
+
+class GeometryError(RuntimeError):
+    pass
+
+
+class Point(object):
+    """ Defines a point with two coordinates. Mimics pya.Point"""
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return self.__class__(x, y)
+
+    def __sub__(self, other):
+        x = self.x - other.x
+        y = self.y - other.y
+        return self.__class__(x, y)
+
+    __array_priority__ = MAGIC_NUMBER  #: This allows rmul to be called first. See https://stackoverflow.com/questions/38229953/array-and-rmul-operator-in-python-numpy"""
+
+    def __mul__(self, factor):
+        """ This implements P * factor"""
+        if isinstance(factor, np.ndarray):
+            # Return a Line instead
+            return Line(self.x * factor, self.y * factor)
+        elif isinstance(factor, Point):
+            return self.x * factor.x + self.y * factor.y
+        return self.__class__(self.x * factor, self.y * factor)
+
+    def __rmul__(self, factor):
+        """ This implements factor * P """
+        if isinstance(factor, np.ndarray):
+            return self.__mul__(factor)
+        return self.__class__(self.x * factor, self.y * factor)
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __str__(self):
+        return f"Point({self.x}, {self.y})"
+
+    def norm(self):
+        return sqrt(self.x**2 + self.y**2)
+
+
+class Line(Point):
+    """ Defines a line """
+
+    def __init__(self, x, y):
+        x, y = np.array(x), np.array(y)
+        if np.shape(x) == np.shape(y):
+            self.x = x
+            self.y = y
+
+    def __eq__(self, other):
+        return np.all(self.x == other.x) and np.all(self.y == other.y)
+
+
+# testing a few operations
+
+a = Point(1, 2)
+assert a * 2 == 2 * a
+
+b = Point(1, 3)
+
+c = a + b
+assert a + b == Point(2, 5)
+assert b - a == Point(0, 1)
+
+t = np.linspace(0, 1, 100)
+assert a * t == t * a
+assert isinstance(a * t, Line)
+
+
+def rotate(point, angle_rad):
+    th = angle_rad
+    x, y = point.x, point.y
+    new_x = x * np.cos(th) - y * np.sin(th)
+    new_y = y * np.cos(th) + x * np.sin(th)
+    return point.__class__(new_x, new_y)
+
+
+rotate90 = lambda point: rotate(point, np.pi / 2)
+
+
+# ####################### ARC METHODS    ##########################
+
+
+# ####################### BEZIER METHODS ##########################
+
+
+def bezier_line(P0, P1, P2, P3):
+    """Cubic Bézier formula
+
+    Returns:
+        Function of parameter t (1d array)
+
+    Reference
+        https://en.wikipedia.org/wiki/Bézier_curve"""
+
+    return lambda t: (1 - t)**3 * P0 + 3 * (1 - t)**2 * t * P1 + 3 * (1 - t) * t**2 * P2 + t**3 * P3
+
+
+def curvature_bezier(P0, P1, P2, P3):
+    """ Measures the curvature of the Bézier curve at every point t
+
+    Returns:
+        Function of parameter t (1d array)
+
+    References:
+        https://en.wikipedia.org/wiki/Radius_of_curvature
+        https://en.wikipedia.org/wiki/Bézier_curve
+    """
+    b_prime = lambda t: 3 * (1 - t)**2 * (P1 - P0) + 6 * (1 - t) * \
+        t * (P2 - P1) + 3 * t**2 * (P3 - P2)
+    b_second = lambda t: 6 * (1 - t) * (P2 - 2 * P1 + P0) + 6 * t * (P3 - 2 * P2 + P1)
+
+    dx = lambda t: b_prime(t).x
+    dy = lambda t: b_prime(t).y
+    ddx = lambda t: b_second(t).x
+    ddy = lambda t: b_second(t).y
+    return lambda t: (dx(t) * ddy(t) - dy(t) * ddx(t)) / (dx(t) ** 2 + dy(t) ** 2) ** (3 / 2)
+
+
+# #### Computing best Bezier curves based on P0, P3, angle0, angle3
+from scipy.optimize import minimize
+
+
+def max_curvature(P0, P1, P2, P3):
+    """Gets the maximum curvature of Bezier curve"""
+    t = np.linspace(0, 1, 300)
+    curv = curvature_bezier(P0, P1, P2, P3)(t)
+    max_curv = np.max(np.abs(curv.flatten()))
+    return max_curv
+
+
+def _curvature_penalty(P0, P1, P2, P3):
+    """Penalty on the curvyness of Bezier curve"""
+    t = np.linspace(0, 1, 300)
+
+    curv = np.abs(curvature_bezier(P0, P1, P2, P3)(t).flatten())
+    max_curv = np.max(curv)
+    curv_initial = curv[0]
+    curv_final = curv[-1]
+
+    # this will cause the minimum curvature to be about 4 times lower
+    # than at the origin and end points.
+    penalty = max_curv + 2 * (curv_initial + curv_final)
+    return penalty
+
+
+def fix_angle(angle):
+    """Returns the angle in the -pi to pi range"""
+    return (angle + np.pi) % (2 * np.pi) - np.pi
+
+
+def logistic_penalty(x, a):
+    return 1 / (1 + np.exp(-x / a))
+
+
+def curve_length(curve, t0=0, t1=1):
+    if isinstance(curve, list):
+        # assuming curve is a list of points
+        scale = (curve[-1] - curve[0]).norm()
+        if scale > 0:
+            coords = np.array([[point.x, point.y] for point in curve]).T
+            dp = np.diff(coords, axis=-1)
+            ds = np.sqrt((dp**2).sum(axis=0))
+            return ds.sum()
+        else:
+            return 0
+    else:
+        # assuming curve is a function.
+        curve_func = curve
+        scale = (curve_func(t1) - curve_func(t0)).norm()
+        if scale > 0:
+            coords = lambda t: np.array([curve_func(t).x, curve_func(t).y])
+            _, sampled_coords = sample_function(coords, [t0, t1], tol=0.0001 / scale, min_points=100)  # 1000 times more precise than the scale
+            dp = np.diff(sampled_coords, axis=-1)
+            ds = np.sqrt((dp**2).sum(axis=0))
+            return ds.sum()
+        else:
+            return 0
+
+
+def _bezier_optimal(angle0, angle3):
+    """ This is a reduced problem of the bézier connection.
+
+    Args:
+        angle0: starting angle in radians
+        angle3: ending angle in radians
+
+    This assumes P0 = (0,0), P3 = (1,0).
+    """
+
+    angle0 = fix_angle(angle0)
+    angle3 = fix_angle(angle3)
+    # print(f"Solving for angles: {angle0}, {angle3}")
+
+    def J(a, b, a_max, b_max):
+        """ Energy function for bezier optimization """
+        P0 = Point(0, 0)
+        P3 = Point(1, 0)
+        P1 = P0 + a * Point(np.cos(angle0), np.sin(angle0))
+        P2 = P3 - b * Point(np.cos(angle3), np.sin(angle3))
+
+        main_penalty = _curvature_penalty(P0, P1, P2, P3)
+
+        # Constraint penalty
+        constraint_penalty = np.exp(-a / 0.05)
+        constraint_penalty += np.exp(-b / 0.05)
+        constraint_penalty += np.exp((a - a_max) / 0.05)
+        constraint_penalty += np.exp((b - b_max) / 0.05)
+
+        # print(f"{a:.2f}, {b:.2f}: {main_penalty}/{constraint_penalty}")
+        return main_penalty + constraint_penalty
+
+    # Initialize problem
+    a = b = 0.3
+    MAX = 1.5
+
+    # If these angles have opposite signs, then calculate the bounds
+    # so that P1 and P2 do not *both* hit the intersection of the
+    # initial tangents. This prevents loops.
+    if angle0 * angle3 < 0 and np.abs(angle3 - angle0) < np.pi:
+        third_angle = np.pi - np.abs(angle3) - np.abs(angle0)
+        a_bound = np.abs(np.sin(angle3)) / np.sin(third_angle)
+        b_bound = np.abs(np.sin(angle0)) / np.sin(third_angle)
+
+        def ineq(a, b):
+            # we want to penalize if both of the following are positive
+            a = a_bound - a
+            b = b_bound - b
+
+            # logistic_penalty(a, 0.0001) * logistic_penalty(b, 0.0001)
+            return 1 * (a > 0) * (b > 0)
+
+        initial_simplex = np.array([[a, b], [a * 1.1, b], [a, b * 1.1]])
+        # print("init ineq:", ineq(a, b))
+        # print("init J:", J(a, b, MAX * 3, MAX * 3))
+        result = minimize(lambda x: J(x[0], x[1], MAX * 3, MAX * 3),
+                          np.array([a, b]),
+                          method='Nelder-Mead',
+                          options=dict(initial_simplex=initial_simplex))
+        # print("end J:", J(result.x[0], result.x[1], MAX * 3, MAX * 3))
+        # print("end ineq:", ineq(result.x[0], result.x[1]))
+    else:
+        # a_bound = MAX / abs(max(np.sin(angle0), np.cos(angle0)))
+        # b_bound = MAX / abs(max(np.sin(angle3), np.cos(angle3)))
+        a_bound = MAX
+        b_bound = MAX
+
+        initial_simplex = np.array([[a, b], [a * 1.1, b], [a, b * 1.1]])
+
+        result = minimize(lambda x: J(x[0], x[1], MAX, MAX),
+                          np.array([a, b]),
+                          method='Nelder-Mead',
+                          options=dict(initial_simplex=initial_simplex))
+
+    if result.success:
+        a, b = result.x[0], result.x[1]
+    else:
+        if result.message == "Maximum number of function evaluations has been exceeded.":
+            a, b = result.x[0], result.x[1]
+        else:
+            print(f"Could not optimize. Exited with message:{result.message}")
+    # print("{:.3f}<{:.3f} {:.3f}<{:.3f}".format(a, a_bound, b, b_bound))
+    return a, b
+
+
+def bezier_optimal(P0, P3, angle0, angle3):
+    """ Computes the optimal bezier curve from P0 to P3 with angles 0 and 3
+
+    Args:
+        P0, P3: Point
+        Angles in degrees
+    """
+
+    angle0 = angle0 * np.pi / 180
+    angle3 = angle3 * np.pi / 180
+
+    vector = P3 - P0
+    angle_m = np.arctan2(vector.y, vector.x)
+    a, b = _bezier_optimal(angle0 - angle_m, angle3 - angle_m)
+
+    scaling = vector.norm()
+    if scaling > 0:
+        P1 = a * scaling * Point(np.cos(angle0), np.sin(angle0)) + P0
+        P2 = P3 - b * scaling * Point(np.cos(angle3), np.sin(angle3))
+        curve_func = bezier_line(P0, P1, P2, P3)
+        with np.errstate(divide='ignore'):
+            print("Min radius: {:.2f} um".format(np.true_divide(1, max_curvature(P0, P1, P2, P3))))
+            print("Total length: {:.3f} um".format(curve_length(curve_func, 0, 1)))
+        return curve_func
+    else:
+        raise GeometryError(f"Error: calling bezier between two identical points: {P0}, {P3}")
+
+
+from functools import partial
+
+
+def bezier_parallel(P0, P3, angle):
+    return bezier_optimal(P0, P3, angle0=angle, angle3=angle)
+
+
+bezier_horizontal = partial(bezier_parallel, angle=0)
+bezier_vertical = partial(bezier_parallel, angle=90)
+
+
+# Allow us to use these functions directly with pya.DPoints
+
+try:
+    import pya
+    _bezier_optimal_pure = bezier_optimal
+
+    def bezier_optimal(P0, P3, *args, **kwargs):
+        P0 = Point(P0.x, P0.y)
+        P3 = Point(P3.x, P3.y)
+        scale = (P3 - P0).norm()  # rough length.
+        # if scale > 1000:  # if in nanometers, convert to microns
+        #     scale /= 1000
+        # This function returns a Line object, needs to convert to array of Points
+        new_bezier_line = _bezier_optimal_pure(P0, P3, *args, **kwargs)
+        bezier_point_coordinates = lambda t: np.array([new_bezier_line(t).x, new_bezier_line(t).y])
+
+        _, bezier_point_coordinates_sampled = \
+            sample_function(bezier_point_coordinates, [0, 1], tol=0.005 / scale)  # tol about 5 nm
+
+        # # This yields a better polygon
+        bezier_point_coordinates_sampled = \
+            np.insert(bezier_point_coordinates_sampled, 1, bezier_point_coordinates(.001 / scale),
+                      axis=1)  # add a point right after the first one
+        bezier_point_coordinates_sampled = \
+            np.insert(bezier_point_coordinates_sampled, -1, bezier_point_coordinates(1 - .001 / scale),
+                      axis=1)  # add a point right before the last one
+        # bezier_point_coordinates_sampled = \
+        #     np.append(bezier_point_coordinates_sampled, np.atleast_2d(bezier_point_coordinates(1 + .001 / scale)).T,
+        #               axis=1)  # finish the waveguide a little bit after
+
+        return [pya.DPoint(x, y) for (x, y) in zip(*(bezier_point_coordinates_sampled))]
+
+except ImportError:
+    pass
+
+# ####################### SIEPIC EXTENSION ##########################
+
+
+class Port(object):
+    """ Defines a port object """
+
+    def __init__(self, name, position, direction, width):
+        self.name = name
+        self.position = position
+        self.direction = direction
+        self.width = width
---
+++ klayout_dot_config/python/SiEPIC/utils/layout.py
@@ -0,0 +1,361 @@
+""" Layout helper functions.
+
+Author: Thomas Ferreira de Lima @thomaslima
+
+The following functions are useful for scripted layout, or making
+PDK Pcells.
+
+TODO: enhance documentation
+TODO: make some of the functions in util use these.
+"""
+
+from itertools import repeat
+import pya
+import numpy as np
+from numpy import cos, sin, pi, sqrt
+from functools import reduce
+from . import sample_function
+from .geometry import rotate90, rotate, bezier_optimal, curve_length
+
+
+def layout_waveguide(cell, layer, points_list, width):
+    """ Lays out a waveguide (or trace) with a certain width with along given points.
+
+    This is very useful for laying out Bezier curves with or without adiabatic tapers.
+
+    Args:
+        cell: cell to place into
+        layer: layer to place into. It is done with cell.shapes(layer).insert(pya.Polygon)
+        points_list: list of pya.DPoint (at least 2 points)
+        width (microns): constant or list. If list, then it has to have the same length as points
+
+    """
+    if len(points_list) < 2:
+        raise NotImplemented("ERROR: points_list too short")
+        return
+
+    try:
+        if len(width) == len(points_list):
+            width_iterator = iter(width)
+        else:
+            width_iterator = repeat(width[0])
+    except TypeError:
+        width_iterator = repeat(width)
+    finally:
+        points_iterator = iter(points_list)
+
+    dbu = cell.layout().dbu
+
+    points_low = list()
+    points_high = list()
+
+    def norm(self):
+        return sqrt(self.x**2 + self.y**2)
+
+    def cos_angle(point1, point2):
+        return point1 * point2 / norm(point1) / norm(point2)
+
+    point_width_list = list(zip(points_iterator, width_iterator))
+    N = len(point_width_list)
+
+    first_point, first_width = point_width_list[0]
+    next_point, next_width = point_width_list[1]
+
+    delta = next_point - first_point
+    theta = np.arctan2(delta.y, delta.x)
+    first_high_point = first_point + 0.5 * first_width * \
+        pya.DPoint(cos(theta + pi / 2), sin(theta + pi / 2))
+    first_low_point = first_point + 0.5 * first_width * \
+        pya.DPoint(cos(theta - pi / 2), sin(theta - pi / 2))
+    points_high.append(first_high_point)
+    points_low.append(first_low_point)
+
+    for i in range(1, N - 1):
+        prev_point, prev_width = point_width_list[i - 1]
+        point, width = point_width_list[i]
+        next_point, next_width = point_width_list[i + 1]
+
+        delta_prev = point - prev_point
+        delta_next = next_point - point
+        theta_prev = np.arctan2(delta_prev.y, delta_prev.x)
+        theta_next = np.arctan2(delta_next.y, delta_next.x)
+
+        next_point_high = (next_point + 0.5 * next_width *
+                           pya.DPoint(cos(theta_next + pi / 2), sin(theta_next + pi / 2)))
+        next_point_low = (next_point + 0.5 * next_width *
+                          pya.DPoint(cos(theta_next - pi / 2), sin(theta_next - pi / 2)))
+
+        forward_point_high = (point + 0.5 * width *
+                              pya.DPoint(cos(theta_next + pi / 2), sin(theta_next + pi / 2)))
+        forward_point_low = (point + 0.5 * width *
+                             pya.DPoint(cos(theta_next - pi / 2), sin(theta_next - pi / 2)))
+
+        prev_point_high = (prev_point + 0.5 * prev_width *
+                           pya.DPoint(cos(theta_prev + pi / 2), sin(theta_prev + pi / 2)))
+        prev_point_low = (prev_point + 0.5 * prev_width *
+                          pya.DPoint(cos(theta_prev - pi / 2), sin(theta_prev - pi / 2)))
+
+        backward_point_high = (point + 0.5 * width *
+                               pya.DPoint(cos(theta_prev + pi / 2), sin(theta_prev + pi / 2)))
+        backward_point_low = (point + 0.5 * width *
+                              pya.DPoint(cos(theta_prev - pi / 2), sin(theta_prev - pi / 2)))
+
+        # High point decision
+        next_high_edge = pya.DEdge(forward_point_high, next_point_high)
+        prev_high_edge = pya.DEdge(backward_point_high, prev_point_high)
+
+        if next_high_edge.crossed_by(prev_high_edge):
+            intersect_point = next_high_edge.crossing_point(prev_high_edge)
+            points_high.append(intersect_point)
+        else:
+            if width * (1 - cos_angle(delta_next, delta_prev)) > dbu:
+                points_high.append(backward_point_high)
+                points_high.append(forward_point_high)
+            else:
+                points_high.append((backward_point_high + forward_point_high) * 0.5)
+
+        # Low point decision
+        next_low_edge = pya.DEdge(forward_point_low, next_point_low)
+        prev_low_edge = pya.DEdge(backward_point_low, prev_point_low)
+
+        if next_low_edge.crossed_by(prev_low_edge):
+            intersect_point = next_low_edge.crossing_point(prev_low_edge)
+            points_low.append(intersect_point)
+        else:
+            if width * (1 - cos_angle(delta_next, delta_prev)) > dbu:
+                points_low.append(backward_point_low)
+                points_low.append(forward_point_low)
+            else:
+                points_low.append((backward_point_low + forward_point_low) * 0.5)
+
+    last_point, last_width = point_width_list[-1]
+    point, width = point_width_list[-2]
+    delta = last_point - point
+    theta = np.arctan2(delta.y, delta.x)
+    final_high_point = last_point + 0.5 * last_width * \
+        pya.DPoint(cos(theta + pi / 2), sin(theta + pi / 2))
+    final_low_point = last_point + 0.5 * last_width * \
+        pya.DPoint(cos(theta - pi / 2), sin(theta - pi / 2))
+    if (final_high_point - points_high[-1]) * delta > 0:
+        points_high.append(final_high_point)
+    if (final_low_point - points_low[-1]) * delta > 0:
+        points_low.append(final_low_point)
+
+    # Append point only if change in direction is less than 120 degrees.
+    def smooth_append(point_list, point):
+        if point_list is None:
+            print(point)
+        if len(point_list) < 1:
+            point_list.append(point)
+            return point_list
+        elif len(point_list) < 2:
+            curr_edge = point - point_list[-1]
+            if norm(curr_edge) > dbu:
+                point_list.append(point)
+                return point_list
+
+        curr_edge = point - point_list[-1]
+        if norm(curr_edge) > dbu:
+            prev_edge = point_list[-1] - point_list[-2]
+            if cos_angle(curr_edge, prev_edge) > cos(120 / 180 * pi):
+                point_list.append(point)
+        return point_list
+
+    polygon_points = points_low + list(reversed(points_high))
+    polygon_points = list(reduce(smooth_append, polygon_points, list()))
+
+    poly = pya.DPolygon(polygon_points)
+    cell.shapes(layer).insert(poly)
+
+
+def layout_ring(cell, layer, center, r, w):
+        # function to produce the layout of a ring
+        # cell: layout cell to place the layout
+        # layer: which layer to use
+        # center: origin DPoint
+        # r: radius
+        # w: waveguide width
+        # units in microns
+
+        # example usage.  Places the ring layout in the presently selected cell.
+        # cell = pya.Application.instance().main_window().current_view().active_cellview().cell
+        # layout_ring(cell, cell.layout().layer(LayerInfo(1, 0)), pya.DPoint(0,0), 10, 0.5)
+
+    layout_arc(cell, layer, center, r, w, 0, 2 * np.pi)
+
+
+def layout_arc(cell, layer, center, r, w, theta_start, theta_end, ex=None):
+    # function to produce the layout of an arc
+    # cell: layout cell to place the layout
+    # layer: which layer to use
+    # center: origin DPoint
+    # r: radius
+    # w: waveguide width
+    # theta_start, theta_end: angle in radians
+    # units in microns
+
+    # example usage.  Places the ring layout in the presently selected cell.
+    # cell = pya.Application.instance().main_window().current_view().active_cellview().cell
+    # layout_arc(cell, layer, pya.DPoint(0,0), 10, 0.5, 0, np.pi/2)
+
+    # fetch the database parameters
+
+    if ex is None:
+        ex = pya.DPoint(1, 0)
+
+    delta_theta = np.arctan2(ex.y, ex.x)
+    theta_start += delta_theta
+    theta_end += delta_theta
+
+    # optimal sampling
+    arc_function = lambda t: np.array([center.x + r * np.cos(t), center.y + r * np.sin(t)])
+    t, coords = sample_function(arc_function,
+                                [theta_start, theta_end], tol=0.002 / r)
+
+    # # This yields a better polygon
+    coords = np.insert(coords, 0, arc_function(theta_start - 0.001),
+                       axis=1)  # start the waveguide a little bit before
+    coords = np.append(coords, np.atleast_2d(arc_function(theta_end + 0.001)).T,
+                       axis=1)  # finish the waveguide a little bit after
+
+    layout_waveguide(cell, layer, [pya.DPoint(x, y) for x, y in zip(*coords)], w)
+
+
+def layout_arc_drc_exclude(cell, drc_layer, center, r, w, theta_start, theta_end, ex=None):
+    corner_points = [center + (r + w / 2) * rotate(ex, theta_start),
+                     center + (r - w / 2) * rotate(ex, theta_start),
+                     center + (r + w / 2) * rotate(ex, theta_end),
+                     center + (r - w / 2) * rotate(ex, theta_end)]
+    for corner_point in corner_points:
+        layout_square(cell, drc_layer, corner_point, 0.1, ex)
+
+
+def layout_arc_with_drc_exclude(cell, layer, drc_layer, center, r, w, theta_start, theta_end, ex=None):
+    layout_arc(cell, layer, center, r, w, theta_start, theta_end, ex)
+    layout_arc_drc_exclude(cell, drc_layer, center, r, w, theta_start, theta_end, ex)
+
+
+def layout_circle(cell, layer, center, r):
+    # function to produce the layout of a filled circle
+    # cell: layout cell to place the layout
+    # layer: which layer to use
+    # center: origin DPoint
+    # r: radius
+    # w: waveguide width
+    # theta_start, theta_end: angle in radians
+    # units in microns
+    # optimal sampling
+
+    arc_function = lambda t: np.array([center.x + r * np.cos(t), center.y + r * np.sin(t)])
+    t, coords = sample_function(arc_function,
+                                [0, 2 * np.pi - 0.001], tol=0.002 / r)
+
+    dbu = cell.layout().dbu
+    dpoly = pya.DPolygon([pya.DPoint(x, y) for x, y in zip(*coords)])
+    cell.shapes(layer).insert(dpoly.to_itype(dbu))
+
+
+def layout_path(cell, layer, point_iterator, w):
+    path = pya.DPath(list(point_iterator), w, 0, 0).to_itype(cell.layout().dbu)
+    cell.shapes(layer).insert(pya.Path.from_dpath(path))
+
+
+def layout_path_with_ends(cell, layer, point_iterator, w):
+    dpath = pya.DPath(list(point_iterator), w, w / 2, w / 2)
+    cell.shapes(layer).insert(dpath)
+
+
+def box_dpolygon(point1, point3, ex=None):
+    # position point2 to the right of point1
+    if ex is None:
+        ex = pya.DPoint(1, 0)
+    ey = rotate90(ex)
+    point2 = point1 * ex * ex + point3 * ey * ey
+    point4 = point3 * ex * ex + point1 * ey * ey
+
+    return pya.DPolygon([point1, point2, point3, point4])
+
+
+def square_dpolygon(center, width, ex=None):
+    # returns the polygon of a square centered at center,
+    # aligned with ex, with width in microns
+    if ex is None:
+        ex = pya.DPoint(1, 0)
+    ey = rotate90(ex)
+    quadrant = (width / 2) * (ex + ey)
+    point1 = center + quadrant
+    quadrant = rotate90(quadrant)
+    point2 = center + quadrant
+    quadrant = rotate90(quadrant)
+    point3 = center + quadrant
+    quadrant = rotate90(quadrant)
+    point4 = center + quadrant
+
+    return pya.DPolygon([point1, point2, point3, point4])
+
+
+def layout_square(cell, layer, center, width, ex=None):
+    """ Lays out a square in the DRC layer
+
+    Args:
+        center: pya.DPoint (um units)
+        width: float (um units)
+        ex: orientation
+
+    """
+
+    if ex is None:
+        ex = pya.DPoint(1, 0)
+
+    square = square_dpolygon(center, width, ex)
+    cell.shapes(layer).insert(square)
+
+
+def append_relative(points, *relative_vectors):
+    """ Appends to list of points in relative steps """
+    try:
+        if len(points) > 0:
+            origin = points[-1]
+    except TypeError:
+        raise TypeError("First argument must be a list of points")
+
+    for vector in relative_vectors:
+        points.append(origin + vector)
+        origin = points[-1]
+    return points
+
+
+def place_cell(parent_cell, cell, placement_origin, params=None, relative_to=None):
+    """ Places a cell and return ports
+    Args:
+        parent_cell: cell to place into
+        cell: cell to be placed
+        placement_origin: pya.Point object to be used as origin
+        relative_to: port name
+
+    Returns:
+        ports(dict): key:port.name, value: geometry.Port with positions relative to parent_cell's origin
+    """
+    layout = parent_cell.layout()
+    pcell, ports = cell.pcell(layout, params=params)
+    if relative_to is not None:
+        offset = next((port.position for port in ports if port.name == relative_to), None)
+        placement_origin = placement_origin - offset
+    parent_cell.insert(pya.CellInstArray(pcell.cell_index(),
+                                         pya.Trans(pya.Trans.R0, placement_origin.to_itype(layout.dbu))))
+    for port in ports:
+        port.position += placement_origin
+
+    return {port.name: port for port in ports}
+
+
+def layout_connect_ports(cell, layer, port_from, port_to):
+
+    P0 = port_from.position
+    P3 = port_to.position
+    angle_from = np.arctan2(port_from.direction.y, port_from.direction.x) * 180 / pi
+    angle_to = np.arctan2(-port_to.direction.y, -port_to.direction.x) * 180 / pi
+
+    curve = bezier_optimal(P0, P3, angle_from, angle_to)
+    layout_waveguide(cell, layer, curve, [port_from.width, port_to.width])
+    return curve_length(curve)
