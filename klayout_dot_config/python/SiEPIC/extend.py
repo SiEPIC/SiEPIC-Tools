5b8b8cc840dba8c7abf29b961d92418b4a4df81d
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-04-18 20:08:14 -0400

    extending point and vector in klayout

--- klayout_dot_config/python/SiEPIC/extend.py
+++ klayout_dot_config/python/SiEPIC/extend.py
@@ -1267,6 +1267,61 @@ def angle_vector(u):
 pya.Point.angle_vector = angle_vector
 pya.DPoint.angle_vector = angle_vector
 
+##########################################################################################
+#                    SiEPIC Class Extension of Point/Vector Classes                      #
+##########################################################################################
+from ._globals import MODULE_NUMPY
+
+if MODULE_NUMPY:
+    import numpy as np
+from numbers import Number
+from math import sqrt
+
+# Point-like classes
+PointLike = (pya.Point, pya.DPoint, pya.DVector, pya.Vector)
+
+
+def pyaPoint__rmul__(self, factor):
+    """ This implements factor * P """
+    if isinstance(factor, Number):
+        return self.__class__(self.x * factor, self.y * factor)
+    elif MODULE_NUMPY and isinstance(factor, np.ndarray):  # ideally this is never called
+        return factor.__mul__(self)
+    else:
+        return NotImplemented
+
+
+def pyaPoint__mul__(self, factor):
+    """ This implements P * factor """
+    if isinstance(factor, Number):
+        return self.__class__(self.x * factor, self.y * factor)
+    elif MODULE_NUMPY and isinstance(factor, np.ndarray):  # Numpy can multiply any object
+        return factor.__mul__(self)
+    elif isinstance(factor, PointLike):
+        return self.x * factor.x + self.y * factor.y
+    else:
+        return NotImplemented
+
+
+def pyaPoint__truediv__(self, dividend):
+    """ This implements P / dividend """
+    return self.__class__(self.x / dividend, self.y / dividend)
+
+
+def pyaPoint_norm(self):
+    """ This implements the L2 norm """
+    return sqrt(self.x ** 2 + self.y ** 2)
+
+
+for klass in PointLike:
+    klass.__rmul__ = pyaPoint__rmul__
+    klass.__mul__ = pyaPoint__mul__
+    klass.__truediv__ = pyaPoint__truediv__
+    klass.norm = pyaPoint_norm
+
+assert pya.DPoint(1, 2) / 1.0 == pya.DPoint(1, 2)
+assert 0.5 * pya.DPoint(1, 2) == pya.DPoint(0.5, 1)
+
 
 '''
 dbu float-int extension:
