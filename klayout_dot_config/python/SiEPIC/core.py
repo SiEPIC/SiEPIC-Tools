SiEPIC/SiEPIC-Tools

commit a3246a99e269f87154c0c729cebd93052ec503eb

fix: Path with 1 vertex on PinRec caused error; check for Path num_points = 2; don't add invalid Pins

klayout_dot_config/python/SiEPIC/core.py
@@ -88,18 +88,6 @@ class Pin():
     def __init__(self, path=None, _type=None, box=None, polygon=None, component=None, net=None, pin_name=None):
         from .utils import angle_vector
         from . import _globals
-        self.path = path            # the pin's Path (Optical)
-        if path:
-            pts = path.get_points()
-            self.center = (pts[0] + pts[1]) * 0.5  # center of the pin: a Point
-            self.rotation = angle_vector(pts[1] - pts[0])  # direction / angle of the optical pin
-        else:
-            self.rotation = 0
-        self.box = box              # the pin's Box (Electrical)
-        if box:
-            self.center = box.center()  # center of the pin: a Point
-        if polygon:
-            self.center = polygon.center()  # center of the pin: a Point (relative coordinates, within component)
         self.type = _type           # one of PIN_TYPES, defined in SiEPIC._globals.PINTYPES
         if net:                     # Net for netlist generation
             self.net = net            # which net this pin is connected to
@@ -107,6 +95,24 @@ class Pin():
             self.net = _globals.NET_DISCONNECTED
         self.pin_name = pin_name    # label read from the cell layout (PinRec text)
         self.component = component  # which component index this pin belongs to
+        self.path = path            # the pin's Path (Optical)
+        if path:
+            pts = path.get_points()
+            if len(pts) == 2:
+              self.center = (pts[0] + pts[1]) * 0.5  # center of the pin: a Point
+            else:
+              print('SiEPIC-Tools: class Pin():__init__: detected invalid Pin')
+              self.rotation = 0
+              return
+            self.rotation = angle_vector(pts[1] - pts[0])  # direction / angle of the optical pin
+        else:
+            self.rotation = 0
+        self.box = box              # the pin's Box (Electrical)
+        if box:
+            self.center = box.center()  # center of the pin: a Point
+        if polygon:
+            self.rotation = 0
+            self.center = polygon.bbox().center()  # center of the pin: a Point (relative coordinates, within component)
 
     def transform(self, trans):
         # Transformation of the pin location

klayout_dot_config/python/SiEPIC/extend.py
@@ -424,13 +424,14 @@ def find_pins(self, verbose=False, polygon_devrec=None):
                 if iter2.shape().is_text():
                     pin_name = iter2.shape().text.string
                 iter2.next()
-            if pin_name == None:
+            if pin_name == None or pin_path.num_points()!=2:
                 print("Invalid pin Path detected: %s. Cell: %s" % (pin_path, subcell.name))
-                error_text += ("Invalid pin Path detected: %s, in Cell: %s, Optical Pins must have a pin name." %
+                error_text += ("Invalid pin Path detected: %s, in Cell: %s, Optical Pins must have a pin name.\n" %
                                (pin_path, subcell.name))
 #        raise Exception("Invalid pin Path detected: %s, in Cell: %s.\nOptical Pins must have a pin name." % (pin_path, subcell.name))
-            # Store the pin information in the pins array
-            pins.append(Pin(path=pin_path, _type=_globals.PIN_TYPES.OPTICAL, pin_name=pin_name))
+            else:
+              # Store the pin information in the pins array
+              pins.append(Pin(path=pin_path, _type=_globals.PIN_TYPES.OPTICAL, pin_name=pin_name))
 
         # Assume a PinRec Box is an electrical pin
         # similar to optical pin
@@ -453,7 +454,7 @@ def find_pins(self, verbose=False, polygon_devrec=None):
                     pin_name = iter2.shape().text.string
                 iter2.next()
             if pin_name == None:
-                error_text += ("Invalid pin Box detected: %s, Cell: %s, Electrical Pins must have a pin name." %
+                error_text += ("Invalid pin Box detected: %s, Cell: %s, Electrical Pins must have a pin name.\n" %
                                (pin_box, subcell.name))
 #        raise Exception("Invalid pin Box detected: %s.\nElectrical Pins must have a pin name." % pin_box)
             pins.append(Pin(box=pin_box, _type=_globals.PIN_TYPES.ELECTRICAL, pin_name=pin_name))
@@ -486,7 +487,7 @@ def find_pins(self, verbose=False, polygon_devrec=None):
                     pin_name = iter2.shape().text.string
                 iter2.next()
             # Store the pin information in the pins array
-            pins.append(Pin(path=it.shape().polygon.transformed(it.itrans()),
+            pins.append(Pin(polygon=it.shape().polygon.transformed(it.itrans()),
                             _type=_globals.PIN_TYPES.OPTICALIO,
                             pin_name=pin_name))
         it.next()

commit 4ae9035dc774532272b65996f9a5c93dc3627ec3

fix for simulation using Laser/Detector; issue introduced in a3246a9

klayout_dot_config/python/SiEPIC/core.py
@@ -96,6 +96,7 @@ class Pin():
         self.pin_name = pin_name    # label read from the cell layout (PinRec text)
         self.component = component  # which component index this pin belongs to
         self.path = path            # the pin's Path (Optical)
+        self.polygon = polygon      # the pin's polygon (Optical IO)
         if path:
             pts = path.get_points()
             if len(pts) == 2:
@@ -122,6 +123,10 @@ class Pin():
             pts = self.path.get_points()
             self.center = (pts[0] + pts[1]) * 0.5
             self.rotation = angle_vector(pts[1] - pts[0])
+        if self.polygon:
+            self.polygon = self.polygon.transformed(trans)
+            self.center = self.polygon.bbox().center()
+            self.rotation = 0
         return self
 
     def display(self):