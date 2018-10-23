c3894111d14cbdad4d3956734ae7fa4c54fbae97
Alex Tait <alexander.tait@nist.gov>
2018-10-23 13:22:22 -0600

    spoofed all GUI features. SiEPIC imports. First test passes.

--- tests/test_0.py
+++ tests/test_0.py
@@ -1,10 +1,4 @@
-import os
-import sys
 
-siepic_package_path = os.path.join(os.path.dirname(__file__), '..', 'klayout_dot_config', 'python')
-sys.path.append(siepic_package_path)
-
-import SiEPIC
 
 def test_first():
-    pass
+    import SiEPIC
