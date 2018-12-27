de9ed608d83f707ee985d8f635233752f7681e19
Thomas Ferreira de Lima <tlima@princeton.edu>
2018-12-27 17:44:33 -0500

    Adding tech utils for technology management from python

--- klayout_dot_config/python/siepic_tools/utils/__init__.py
+++ klayout_dot_config/python/siepic_tools/utils/__init__.py
@@ -1,3 +1,6 @@
 '''
 Utilities module for siepic_tools
 '''
+
+from .tech import get_technology_by_name  # noqa
+from .tech import register_siepic_technology  # noqa
--- klayout_dot_config/python/siepic_tools/utils/tech.py
+++ klayout_dot_config/python/siepic_tools/utils/tech.py
@@ -1,4 +1,92 @@
-from pya import LayerInfo
+from __future__ import print_function
+from os.path import expanduser, realpath, dirname
+from pya import \
+    LayerInfo, \
+    Technology
+
+# Module attributes
+
+technologies = {}
+
+# Technology management
+
+
+def get_technology_by_name(name):
+    try:
+        return technologies[name]
+    except KeyError:
+        print("Technology %s not found." % name)
+        raise
+
+
+def register_siepic_technology(lyt_filename):
+    # Loading db Technology from file
+    pya_tech = _load_pya_tech(lyt_filename)
+
+    # Registering technology in db Technology
+    registered_pya_tech = _register_pyatech(pya_tech)
+
+    tech_name = pya_tech.name
+    # Registering technology in siepic_tools.utils.tech.technologies
+    technologies[tech_name] = _load_siepic_technology(registered_pya_tech)
+
+    return technologies[tech_name]
+
+
+def _load_siepic_technology(pya_tech):
+    technology = {}
+    tech_name = pya_tech.name
+    technology['technology_name'] = tech_name
+    technology['dbu'] = pya_tech.dbu
+
+    import os
+    lyp_file = pya_tech.eff_layer_properties_file()
+    technology['base_path'] = pya_tech.base_path()
+
+    # Load CML file location
+    head, tail = os.path.split(lyp_file)
+    technology['base_path'] = head
+    cml_files = [x for x in os.listdir(technology['base_path']) if x.lower().endswith(".cml")]
+    if cml_files:
+        technology['INTC_CML'] = cml_files[-1]
+        technology['INTC_CML_path'] = os.path.join(technology['base_path'], cml_files[-1])
+        technology['INTC_CML_version'] = cml_files[-1].replace(tech_name + '_', '')
+    else:
+        technology['INTC_CML'] = ''
+        technology['INTC_CML_path'] = ''
+        technology['INTC_CML_version'] = ''
+
+    # Layers:
+    layer_map = parse_layer_map(lyp_file)
+    technology.update(layer_map)
+
+    return technology
+
+
+def _load_pya_tech(lyt_filename):
+    # Parsing lyt filename
+
+    # workaround while https://github.com/klayoutmatthias/klayout/pull/215 is not solved
+    absolute_filepath = realpath(expanduser(lyt_filename))
+    with open(absolute_filepath, 'r') as file:
+        lyt_xml = file.read()
+    pya_tech = Technology.technology_from_xml(lyt_xml)
+    pya_tech.default_base_path = dirname(absolute_filepath)
+    # end of workaround
+
+    return pya_tech
+
+
+def _register_pyatech(pya_tech):
+    if Technology.has_technology(pya_tech.name):
+        print("Warning: overwriting %s technology" % pya_tech.name)
+        new_tech = Technology.technology_by_name(pya_tech.name)
+    else:
+        new_tech = Technology.create_technology(pya_tech.name)
+
+    # Registering new technology to klayout's database
+    new_tech.assign(pya_tech)
+    return new_tech
 
 # Layer Properties
 
