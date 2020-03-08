65e49989f51ec9995b6ad7c2c01821294b579caa
Lukas Chrostowski <lukasc@ece.ubc.ca>
2020-03-08 01:15:34 -0800

    v0.3.62

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.61'
+__version__ = '0.3.62'
 #from . import install, extend, _globals, core, examples, github, lumerical, scripts, utils, setup
 # from . import install, extend, _globals, core, examples, github, scripts, utils, setup
 from . import extend, _globals, core, examples, github, scripts, utils, setup, install
