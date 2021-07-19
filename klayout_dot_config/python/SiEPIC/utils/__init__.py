8d29fa438e959559c7b8e8eae11ad5b0dd6736d8
Sean L <seanlam97@gmail.com>
2021-07-19 15:41:11 -0700

    [loadWG] Fix function to handle single wg in XML file

--- klayout_dot_config/python/SiEPIC/utils/__init__.py
+++ klayout_dot_config/python/SiEPIC/utils/__init__.py
@@ -347,8 +347,9 @@ def load_Waveguides_by_Tech(tech_name, debug=False):
         for path1 in paths:
             with open(path1, 'r') as file:
                 waveguides1 = xml_to_dict(file.read())
+                print(waveguides1)
                 try:
-                    waveguides += waveguides1['waveguides']['waveguide']
+                    waveguides.append(waveguides1['waveguides']['waveguide'])
                 except:
                     pass
         for waveguide in waveguides:
