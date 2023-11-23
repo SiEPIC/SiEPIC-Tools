44e51a3acb3e30dccb25e6d1ea7751ef242bf7a5
jasminabrar <73445895+jasminabrar@users.noreply.github.com>
2023-11-23 09:16:24 -0800

    update to match main

--- klayout_dot_config/python/SiEPIC/_globals.py
+++ klayout_dot_config/python/SiEPIC/_globals.py
@@ -1,38 +1,31 @@
 
+import pya
 Python_Env = "" # tag which defines whether we are loading library in script or GUI env
-try:
-    # import pya, which is available  running within KLayout
-    import pya
-    if pya.Application.instance().main_window():
-        Python_Env = "KLayout_GUI"
-        print('Python Environment: KLayout GUI')
-    else:
-        Python_Env = "KLayout_batch"
-        print('Python Environment: KLayout batch mode')
-except:
+if 'Application' in dir(pya):
+    try:
+        # import pya, which is available when running within KLayout
+        if pya.Application.instance().main_window():
+            Python_Env = "KLayout_GUI"
+            print('Python Environment: KLayout GUI')
+        else:
+            Python_Env = "KLayout_batch"
+            print('Python Environment: KLayout batch mode')
+    except:
+        Python_Env = "Script"
+else:
     Python_Env = "Script"
-'''
-if Python_Env == "KLayout_GUI":
-    # Netlist extraction will merge straight+bend sections into waveguide (1),
-    # or extract each bend, straight section, etc. (0)
-    #WAVEGUIDE_extract_simple = 1
-    SIMPLIFY_NETLIST_EXTRACTION = True
 
-    from .core import WaveguideGUI, MonteCarloGUI, Net, Component
-'''
 # Netlist extraction will merge straight+bend sections into waveguide (1),
 # or extract each bend, straight section, etc. (0)
 #WAVEGUIDE_extract_simple = 1
 SIMPLIFY_NETLIST_EXTRACTION = True
     
 if Python_Env == "KLayout_GUI":
-
     # Create GUI's
     from .core import WaveguideGUI, MonteCarloGUI
     WG_GUI = WaveguideGUI()
     MC_GUI = MonteCarloGUI()
-
-
+    
 # ******** lukasc
 # don't use a global one.. based on cells
 # Define global Net object that implements netlists and pin searching/connecting
@@ -57,35 +50,45 @@ def enum(*sequential, **named):
 PIN_TYPES = enum('OPTICALIO', 'OPTICAL', 'ELECTRICAL')
 PIN_LENGTH = 20  # 10 nm on each side. Previous was 2x50 nm, but shorter works well for Waveguide DRC checking
 
-if Python_Env == "KLayout_GUI":
-    MODULE_NUMPY = False
+
+MODULE_NUMPY = False
+try:
+    import numpy
+    MODULE_NUMPY = True
+except ImportError:
+    from .install import install
     try:
-        import numpy
-        MODULE_NUMPY = True
-    except ImportError:
-        from .install import install_numpy
-        try:
-            
-            import numpy
-            MODULE_NUMPY = True
-        except ImportError:
-            from .install import install_numpy
-            try:
-                MODULE_NUMPY = install_numpy()
-            except Exception as e:
-                print("Could not install numpy with pip. ERROR:", e)
+        MODULE_NUMPY = install('numpy')
+    except Exception as e:
+        print("Could not install numpy with pip. ERROR:", e)
+
+# scipy
+try:
+    import scipy
+except ImportError:
+    from .install import install
+    try:
+        install('scipy')
+    except Exception as e:
+        print("Could not install scipy with pip. ERROR:", e)
+
 
 #ACTIONS = []
 
-
-KLAYOUT_VERSION = int(pya.Application.instance().version().split('.')[1])
-KLAYOUT_VERSION_3 = int(pya.Application.instance().version().split('.')[2])
+if '__version__' in dir(pya):
+    # pya.__version__ was introduced in KLayout version 0.28.6
+    KLAYOUT_VERSION = int(pya.__version__.split('.')[1])
+    KLAYOUT_VERSION_3 = int(pya.__version__.split('.')[2])
+else:
+    KLAYOUT_VERSION = int(pya.Application.instance().version().split('.')[1])
+    KLAYOUT_VERSION_3 = int(pya.Application.instance().version().split('.')[2])
 
 # Waveguide DevRec: space between the waveguide and the DevRec polygon
 WG_DEVREC_SPACE = 1
 
 # Path to Waveguide, path snapping to nearest pin. Search for pin with this distance:
 PATH_SNAP_PIN_MAXDIST = 20
+
     
 INTC_ELEMENTS = ''
 if Python_Env == "KLayout_GUI":
@@ -123,9 +126,6 @@ if Python_Env == "KLayout_GUI":
         LUMAPI = None
         print('resetting Lumerical Python integration')
     
-    
-try:
-    TEMP_FOLDER
-except:
+if 'TEMP_FOLDER' not in locals(): 
     import tempfile
     TEMP_FOLDER = tempfile.mkdtemp()
