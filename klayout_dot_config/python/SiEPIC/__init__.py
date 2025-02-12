8a7275280ac6db355e2d5d3d6aa235f594466fe8
Lukas Chrostowski <lukasc@ece.ubc.ca>
2025-02-12 00:29:04 -0800

    function connect_pins_with_waveguide: find parents
    when looking for mutual parent for two cells, scan the entire tree hierarchy.
    Code generated by ChatGPT 4o

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -2,7 +2,7 @@
 SiEPIC-Tools package for KLayout
 '''
 
-__version__ = "0.5.23"
+__version__ = "0.5.243"
 
 print("KLayout SiEPIC-Tools version %s" %__version__)
 
--- klayout_dot_config/python/SiEPIC/scripts.py
+++ klayout_dot_config/python/SiEPIC/scripts.py
@@ -188,6 +188,66 @@ def connect_pins_with_waveguide(instanceA, pinA, instanceB, pinB, waveguide = No
                         pass                
         else:
                 parentA=''
+        if verbose:
+            print('%s, %s: %s' % (instanceB.parent_cell, instanceB.parent_cell.parent_cells(), ''))
+                        
+        def find_parents(layout, target_cell):
+            """
+            Find all parent cells that instantiate the target cell.
+            """
+            parents = []
+            for cell in layout.each_cell():
+                for inst in cell.each_inst():
+                    if inst.cell == target_cell:
+                        parents.append(cell)
+                        break
+            return parents
+
+        def trace_hierarchy_up(layout, bottom_cell, path=None):
+            """
+            Recursively trace the hierarchy upwards from the bottom cell to the top cell.
+            """
+            if path is None:
+                path = [bottom_cell]
+
+            parents = find_parents(layout, bottom_cell)
+            if not parents:
+                # No parents found, we've reached the top cell
+                return [path]
+
+            all_paths = []
+            for parent in parents:
+                # Recursively trace each parent
+                new_path = [parent] + path
+                all_paths.extend(trace_hierarchy_up(layout, parent, new_path))
+
+            return all_paths
+
+        def trace_hierarchy_up_single(layout, bottom_cell, path=None):
+            """
+            Recursively trace the hierarchy upwards from the bottom cell to the top cell.
+            Returns a single path as a list of Cell objects.
+            """
+            if path is None:
+                path = [bottom_cell]
+
+            parents = find_parents(layout, bottom_cell)
+            if not parents:
+                # No parents found, we've reached the top cell
+                return path[::-1]  # Reverse the path to start from the top cell
+
+            if len(parents) > 1:
+                raise ValueError(f"Cell '{bottom_cell.name}' has multiple parent instances. Use the multi-path version.")
+
+            # If only one parent, continue tracing
+            return trace_hierarchy_up_single(layout, parents[0], path + [parents[0]])
+
+        parentsA = trace_hierarchy_up_single(ly, instanceA.parent_cell)
+        parentsB = trace_hierarchy_up_single(ly, instanceB.parent_cell)
+        if verbose:
+            print(" -> ".join(cell.name for cell in parentsA))
+            print(" -> ".join(cell.name for cell in parentsB))
+    
         if instanceB.parent_cell.parent_cells() == 1:
                 # find parent parent cell
                 iterB=instanceB.parent_cell.each_parent_cell()
@@ -203,8 +263,6 @@ def connect_pins_with_waveguide(instanceA, pinA, instanceB, pinB, waveguide = No
         else:
                 parentB=''
         # find the common parent
-        parentsA = [instanceA.parent_cell, parentA]
-        parentsB = [instanceB.parent_cell, parentB]
         common_cell = list(set(parentsA).intersection(parentsB))
         if verbose:
             print('%s, %s: %s' % (parentsA, parentsB, common_cell))
@@ -345,7 +403,7 @@ def connect_pins_with_waveguide(instanceA, pinA, instanceB, pinB, waveguide = No
         waveguides = ly.load_Waveguide_types()
         if verbose:
             print('Time elapsed, to load waveguide types: %s' % (time() - t))    
-            print(waveguides)
+        #    print(waveguides)
         if not(waveguide_type):
             waveguide = waveguides[0]
         if waveguide_type:
